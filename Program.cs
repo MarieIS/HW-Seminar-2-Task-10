43b0dc8e5fdd90e51177979854ce213797e0d845
Maria <mariya-ivanova@list.ru>
2024-03-06 21:02:28 +0200

    the solution was just added

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Insert a three digit number: ");
+int N = int.Parse(Console.ReadLine());
+if (N / 100 == 0)
+ {
+   Console.WriteLine($"{N} is not the three digit number. please try again");
+ }
+ else
+ {
+   int digit2 = (N - (N / 100) * 100) / 10;
+   Console.WriteLine($"{N} -> {digit2}");
+ }
